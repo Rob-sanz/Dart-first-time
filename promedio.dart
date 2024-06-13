80ec85d85ba1803868d137add85673b769d2d2b6
rob_sanz <robertosanchez@unah.hn>
2024-06-13 00:12:26 -0600

    Primer ejercicio, muy facil

---
+++ promedio.dart
@@ -0,0 +1,9 @@
+void main(){
+
+  //Programa que devuelva el promedio de 3 numeros
+  final num1=3,num2=4,num3=5, prom;
+
+  prom = (num1+num2+num3)/3;
+
+  print('El PROMEDIO de $num1 , $num2 , $num3  es: $prom');
+}
