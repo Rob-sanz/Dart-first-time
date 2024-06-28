8a89ee30232b1b6c045d02d186356436b32b21c6
rob_sanz <robertosanchez@unah.hn>
2024-06-28 01:05:43 -0600

    Hola Dart :)

---
+++ Hola_Dart.dart
@@ -0,0 +1,20 @@
+void main() {
+
+  var miname = 'Naruto Uzumaki';
+  final miname2 = 'Roberto Sanchez';
+  String miname3 = 'John Alejandro';
+
+  //Esto es un comentario
+  //Usemos late para asignar los valores despues 
+
+  const miname4 = 'fernando';
+
+
+  print('Mi nombre es $miname');
+  print('Mi nombre es $miname2');
+  print('Mi nombre es ${miname3.toUpperCase()}');
+
+
+
+
+}
