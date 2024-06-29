a02cbb5207cce3f7ad08bd926d7e4dc870087c38
rob_sanz <robertosanchez@unah.hn>
2024-06-29 04:21:43 -0600

    App Contador de Clicks

--- Aplicaciones/hello_world_app/lib/main.dart
+++ Aplicaciones/hello_world_app/lib/main.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world_app/presentacion/Pantallas/contadores/contador_pantalla.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home : Center(child: Text('Hola Mundo'))
+      title: 'Mi primer App',
+      debugShowCheckedModeBanner: false,  //Quita el "Debug" de la esquina superior derecha de la App
+      theme: ThemeData( 
+        useMaterial3: true,
+        colorSchemeSeed: Colors.blue
+        ),
+      home : const ContadorPantalla() //Llama a otra clase (Pantalla, Scaffold) creada
 
     );
   }
---
+++ Aplicaciones/hello_world_app/lib/presentacion/Pantallas/contadores/contador_pantalla.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class ContadorPantalla extends StatefulWidget {
+  const ContadorPantalla({super.key});
+
+  @override
+  State<ContadorPantalla> createState() => _ContadorPantallaState();
+}
+
+class _ContadorPantallaState extends State<ContadorPantalla> {
+
+  int contadorDeClicks = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(
+        backgroundColor: Colors.blueGrey[100],
+      ),
+      appBar: AppBar(
+        title: const Text('Pantalla Contador'),
+        centerTitle: true,  //Centra el titulo en appBar
+        backgroundColor: Colors.blueGrey[500],  //Cambiar color del appBarr
+      ),
+        body: Center( child: 
+        Column( mainAxisAlignment: MainAxisAlignment.center, 
+        children: [
+          Text('$contadorDeClicks', style: const TextStyle(fontSize: 160 , fontWeight: FontWeight.w200),),
+          const Text('Cantidad de Clicks', style: TextStyle(fontSize:40 , fontWeight: FontWeight.w200),)
+        ],
+        ),
+      ),
+      
+      floatingActionButton: FloatingActionButton(onPressed: (){
+        contadorDeClicks++;
+        setState(() {});  //Hace que el widyet se renderice cada vez que "contadorDeClicks++"
+      },
+      backgroundColor: Colors.blueGrey[100], //Cambia el color del button
+      child: const Icon(Icons.plus_one),  //Agrega Icono al button  
+      ),
+
+
+    );
+  }
+}
